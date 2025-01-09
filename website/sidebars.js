7aa7cd3f34283e6f86e129eaa8db6030a19ef5c0
Lyudmil Ivanov <55487633+flmel@users.noreply.github.com>
2025-01-09 17:26:25 +0200

    add website

---
+++ website/sidebars.js
@@ -0,0 +1,39 @@
+const sidebar = {
+  intro: [
+    "welcome",
+    {
+      type: "html",
+      value: "<hr/>",
+    },
+    {
+      type: "category",
+      label: "NEAR Components / BOS",
+      items: [
+        "components/what-is",
+        "components/state",
+        "components/dev-environment",
+        "components/integrate-components",
+        "components/interaction",
+        "components/blog-posts",
+        "components/ethers-js",
+        "components/ethers-js-best-practices",
+        "components/bos-gateway",
+        "components/bos-loader",
+        "components/builtin-components",
+        "components/ds-components",
+        "components/using-iframes",
+        "components/lido",
+        "components/near",
+        "components/notifications",
+        "components/push-notifications",
+        "components/social",
+        "components/web-methods",
+        "components/chain-hosted-ui",
+        "components/chain-hosted-ui-tutorial"
+      ],
+    },
+  ],
+
+};
+
+export default sidebar;
